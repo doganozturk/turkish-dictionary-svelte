1bc877f15e2a18238ac873e2959744ddd0db68fc
Doğan Öztürk <doganozturk2005@gmail.com>
2020-05-15 14:20:50 +0300

    Add routing support.

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,20 @@
 <script>
-    export let name;
+    import { Router, Route, Link } from 'svelte-routing';
+    import Home from './routes/Home.svelte';
+    import Detail from './routes/Detail.svelte';
+
+    export let url = "";
 </script>
 
-<style>
-    main {
-        text-align: center;
-        padding: 1em;
-        max-width: 240px;
-        margin: 0 auto;
-    }
-
-    h1 {
-        color: #ff3e00;
-        text-transform: uppercase;
-        font-size: 4em;
-        font-weight: 100;
-    }
-
-    @media (min-width: 640px) {
-        main {
-            max-width: none;
-        }
-    }
-</style>
-
 <main>
-    <h1>Hello {name}!</h1>
-    <p>
-        Visit the
-        <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-        to learn how to build Svelte apps.
-    </p>
+    <Router {url}>
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="detail">Detail</Link>
+        </nav>
+        <div>
+            <Route path="detail" component={Detail} />
+            <Route path="/" component={Home} />
+        </div>
+    </Router>
 </main>
